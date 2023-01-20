ed4d1d68f219eec0f8d338a505a235d84f1e7ed7
smhrd <smhrd@59.0.236.216>
2023-01-20 10:10:28 +0900

    2023-01-20 / hs / 반복문 추가

--- Ex12GitTest/src/조건문/ex01조건문.java
+++ Ex12GitTest/src/조건문/ex01조건문.java
@@ -3,8 +3,9 @@ package 조건문;
 public class ex01조건문 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		if (true) {
+			System.out.println("조건문");
+		}
 	}
 
 }
---
+++ Ex12GitTest/src/조건문/ex02반복문.java
@@ -0,0 +1,10 @@
+package 조건문;
+
+public class ex02반복문 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
