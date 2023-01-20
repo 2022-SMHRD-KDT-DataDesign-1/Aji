a5412be211fec16d6052df6ade6fb51b157f3c43
smhrd <smhrd@121.147.52.156>
2023-01-20 12:49:00 +0900

    2023 - 01 - 20 정승원 출력문 추가

--- Aji/src/View/LoginPage.java
+++ Aji/src/View/LoginPage.java
@@ -7,6 +7,7 @@ public class LoginPage {
 		System.out.println("이번엔 제가 해봤습니다.");
 		System.out.println("아지는 매우 귀엽습니다.");
 		System.out.println("강아지? 망아지? 송아지?");
+		System.out.println("그중 최고의 아지는 민지씨의 아지라더라");
 		
 	}
 
