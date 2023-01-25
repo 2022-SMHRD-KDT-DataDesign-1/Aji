8902f6f87b53004078e8c0a3c11d0f79f0e19dca
안민지 <@DESKTOP-S5HBU9C>
2023-01-25 12:16:05 +0900

    2023/1/25/안민지/프로젝트생성

---
+++ 아지키우기/src/아지콩/JoinDAO.java
@@ -0,0 +1,5 @@
+package ¾ÆÁöÄá;
+
+public class JoinDAO {
+
+}
---
+++ 아지키우기/src/아지콩/JoinDTO.java
@@ -0,0 +1,17 @@
+package ¾ÆÁöÄá;
+
+public class JoinDTO {
+	
+	 private String id;
+	   private String pw;
+	   private String name;
+
+
+	   public JoinDTO(String id, String pw, String name) {
+
+		      this.id = id;
+		      this.pw = pw;
+		      this.name = name;
+
+}
+}
